7a1255d4a425804968073feec242f65ecaa9eb1a
ghorsey <geoff@shhhinnovations.com>
2013-06-06 09:28:23 -0700

    initial commit

---
+++ src/FileVer/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FileVer
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    /// <summary>
+    /// The <c>FileVer.exe</c> program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Mains the specified args.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <returns>The exit code of the application</returns>
+        public static int Main(string[] args)
+        {
+            var command = Args.Configuration.Configure<ProgramArgs>().CreateAndBind(args);
+            var path = args[0];
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("{0} was not found", path);
+                return -10;
+            }
+
+            var asm = Assembly.LoadFile(Path.GetFullPath(path));
+
+            if (command.Name)
+            {
+                Console.WriteLine(asm.FullName);
+            }
+
+            if (command.Version)
+            {
+                Console.WriteLine(asm.GetName().Version.ToString());
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/FileVer/ProgramArgs.cs
@@ -0,0 +1,24 @@
+﻿namespace FileVer
+{
+    /// <summary>
+    /// The command line arguments for the program
+    /// </summary>
+    public class ProgramArgs
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ProgramArgs"/> is version.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if only the version of the assembly should be written out; otherwise, <c>false</c>.
+        /// </value>
+        public bool Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ProgramArgs"/> is name.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if name; otherwise, <c>false</c>.
+        /// </value>
+        public bool Name { get; set; }
+    }
+}
